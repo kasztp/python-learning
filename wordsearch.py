363b152c6c74b1f3aa71dbd70b44357bc117aebb
kasztp <kasztp@gmail.com>
2019-02-28 08:30:38 +0100

    with function definition

--- wordsearch.py
+++ wordsearch.py
@@ -1,7 +1,14 @@
 filename = 'words.txt'
 db = open(filename, encoding='utf-8')
 words = db.read().splitlines()
-if "apple" in words:
-    print("Apple found in word database!")
-db.close()
+keyword = 'apple'
 
+
+def findword(word):
+    if word in words:
+        print('{} found in word database!'.format(word))
+
+
+findword(keyword)
+
+db.close()
